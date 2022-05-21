5c8970e00f79fc029c3c61d9d38f8226a475bcee
luoshanjie <luoshanjie@gmail.com>
2022-05-21 22:20:12 +0800

    [fix] check http cmd or use curl

--- scripts/conn.sh
+++ scripts/conn.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
-http http://127.0.0.1:10086
+
+echo "test kratos is connected"
+if type http >/dev/null 2>&1; then
+    http http://127.0.0.1:10086
+else
+    curl http://127.0.0.1:10086
+fi
