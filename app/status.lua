53c85c235f52b675b09b834b37582f83f6128489
luoshanjie <luoshanjie@gmail.com>
2022-05-26 09:52:38 +0800

    [refactoring] move config dir

--- app/status.lua
+++ app/status.lua
@@ -4,5 +4,5 @@ local string_format = string.format
 
 
 local value = os.date("%Y-%m-%d %H:%M", os.time())
-ngx.say("Kratos is sample API Gateway at " .. value)
+ngx.say("Kratos Is Quick API Gateway At " .. value)
 cc_printinfo(string_format('返回时间: %s', value))
--- scripts/conn.sh
+++ scripts/conn.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 # echo "test kratos is connected"
-# if type http >/dev/null 2>&1; then
-#     http http://127.0.0.1:10086
-# else
-#     curl http://127.0.0.1:10086
-# fi
+if type http >/dev/null 2>&1; then
+    http http://127.0.0.1:10086
+else
+    curl http://127.0.0.1:10086
+fi
 
 # curl --request POST 'localhost/api/test' \
 # --header 'Content-Type: application/json' \
